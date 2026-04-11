9d1a8385019bb8f2fa30c0919338ca55431178d7
Georgi Katushev <g.katushev@student.fontys.nl>
2026-04-11 21:43:04 +0200

    Test: secret should be blocked

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCY"
 password = "mysupersecretpassword123"
-api_key = "sk-abcdefghijklmnopqrstuvwxyz123456"
+api_key = "sk-abcdefghijklmnopqrstuvwxyz123453"
